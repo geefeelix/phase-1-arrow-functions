d18dbe3714ac99701de9089196a87ee6c2d74e33
godspower <geefeelix@gmail.com>
2022-04-29 13:52:25 -0400

    commit tree 6c7f4d22118c2d92faf1409226096b1994283d84

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (a, b) => {
+    console.log(`Divide ${a}`);
+    console.log(`Divide ${b}`);
+    return a / b;
+}
+
+const square = (x) => {
+    console.log(`Multiply ${x}`);
+    console.log(`Multiply ${x}`);
+    return 2 * 2;
+}
+
+const add = (a, b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
+
